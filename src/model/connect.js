68cacd05e19e3e60073faa6b6b4d74c98ae09080
LeCongNam <namlem4u@gmail.com>
2022-01-17 06:21:31 +0700

    format code

--- src/model/connect.js
+++ src/model/connect.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect(`
-        mongodb+srv://lecongnam:Vobichuyen0612@cluster0.j3gct.mongodb.net/${clean_blog}?retryWrites=true&w=majority`)
+        await mongoose.connect(`mongodb+srv://lecongnam:Vobichuyen0612@cluster0.j3gct.mongodb.net/${clean_blog}?retryWrites=true&w=majority`)
         console.log("Connected to mongoose successfully")
     } catch (error) {
         console.log("Connect Failuse!!!",error)
